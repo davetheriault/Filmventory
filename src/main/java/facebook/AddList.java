3c7f0f53f27af1a8d6c7070d59c82b6f3e43d516
davault <davetheriault@gmail.com>
2016-07-11 22:31:00 -0600

    side nav

--- src/main/java/facebook/AddList.java
+++ src/main/java/facebook/AddList.java
@@ -63,18 +63,20 @@ public class AddList extends HttpServlet {
         String year = request.getParameter("year");
         String listname = request.getParameter("listname");
         String fb_id = (String) request.getSession().getAttribute("id");
-        String page = request.getParameter("page");
+        String page;
+        if(request.getParameter("page") != null) {
+            page = request.getParameter("page");
+        }
 
         JDBC db = new JDBC();
 
         db.addList(fb_id, title, year, listname);
 
-        if (page.equals("moviedetails")) {
-            RequestDispatcher dispatcher = request.getRequestDispatcher("MovieDetails");
-            dispatcher.forward(request, response);
-        }
         if (page.equals("mylists")) {
             RequestDispatcher dispatcher = request.getRequestDispatcher("MyLists");
+            dispatcher.forward(request, response);
+        } else {
+            RequestDispatcher dispatcher = request.getRequestDispatcher("MovieDetails");
             dispatcher.forward(request, response);
         }
     }
